0f96411f388199e1abd5c4953a7822675bc154f7
Nischay Venkatram <nischay.v@gmail.com>
2016-10-29 13:10:05 -0500

    Created basic home page

---
+++ js/containers/HomeContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+    Image
+} from 'react-native';
+import { Examples } from '@shoutem/ui';
+
+export default class HomeContainer extends Component {
+    render() {
+        return (
+            <Examples/>
+        );
+    }
+}
